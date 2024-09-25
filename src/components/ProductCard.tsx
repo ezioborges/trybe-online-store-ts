e2f18de690e4a7ca06f5156b0fe43b1487b61544
Ezio Borges <eziopborges@gmail.com>
2024-09-25 13:05:18 -0300

    ajustes no componente ProductCard

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,13 +1,16 @@
 import { ProcutsCardType } from "../types";
 
+import "../styles/product-card.css";
+
 function ProductCard({ title, img, price }: ProcutsCardType) {
-    return (
-        <div className="border p-3 rounded">
-            <p>{title}</p>
-            <img src={img} alt={`foto do produto ${title}`} />
-            <p>{price}</p>
-        </div>
-    );
- }
+  const productPrice = price.toFixed(2).replace(".", ",");
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center border p-3 rounded product-card">
+      <p className="text-center">{title}</p>
+      <img src={img} alt={`foto do produto ${title}`} className="mt-1" />
+      <p className="mt-1">R$ {productPrice}</p>
+    </div>
+  );
+}
 
 export default ProductCard;
